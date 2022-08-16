1c8aa0d775deaf3d2edff44809bfbdb63f8fbd9e
kazakov93msk <kav.learning@yandex.ru>
2022-08-16 20:23:11 +0300

    fix: Fix old tests

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -3,8 +3,14 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import ru.yandex.practicum.filmorate.utility.IdentifierGenerator;
 
 import java.time.LocalDate;
@@ -13,35 +19,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FilmControllerTest {
-//    FilmController filmController;
-//
-//    @BeforeEach
-//    public void beforeEach() {
-//        filmController = new FilmController(new IdentifierGenerator());
-//    }
-//
-//    @Test
-//    public void shouldReturnValidationExceptionWhenFilmInvalid() {
-//        Film filmFrom1890 = new Film("Very Old Film", "Cinema not was invented!",
-//                100, LocalDate.of(1800, 1, 1));
-//        Film filmWithInvalidId = new Film("Invalid Identifier", "Id Invalid",
-//                100, LocalDate.of(2000, 1, 1));
-//        filmWithInvalidId.setId(-1);
-//
-//        assertThrows(ValidationException.class, () -> filmController.create(filmFrom1890),
-//                "Ожидалось ValidationException, возвращено некорректное исключение");
-//        assertThrows(ValidationException.class, () -> filmController.create(filmWithInvalidId),
-//                "Ожидалось ValidationException, возвращено некорректное исключение");
-//    }
-//
-//    @Test
-//    public void shouldReturnFilmAfterAdded() {
-//        Film validFilm = new Film("Film", "Descr",
-//                120, LocalDate.of(2000, 1, 1));
-//        Film returnedFilm = filmController.create(validFilm);
-//        validFilm.setId(1);
-//        assertEquals(validFilm, returnedFilm,
-//                "Добавленный фильм не совпадает с изначальным");
-//    }
+    FilmController filmController;
+    UserStorage userStorage;
+    FilmStorage filmStorage;
+    FilmService filmService;
+    @BeforeEach
+    public void beforeEach() {
+        userStorage = new InMemoryUserStorage(new IdentifierGenerator());
+        filmStorage = new InMemoryFilmStorage(new IdentifierGenerator());
+        filmService = new FilmService(filmStorage, userStorage);
+        filmController = new FilmController(filmStorage, filmService, userStorage);
+    }
+
+    @Test
+    public void shouldReturnValidationExceptionWhenFilmInvalid() {
+        Film filmFrom1890 = new Film("Very Old Film", "Cinema not was invented!",
+                100, LocalDate.of(1800, 1, 1));
+        Film filmWithInvalidId = new Film("Invalid Identifier", "Id Invalid",
+                100, LocalDate.of(2000, 1, 1));
+        filmWithInvalidId.setId(-1);
+
+        assertThrows(ValidationException.class, () -> filmController.create(filmFrom1890),
+                "Ожидалось ValidationException, возвращено некорректное исключение");
+        assertThrows(NotFoundException.class, () -> filmController.create(filmWithInvalidId),
+                "Ожидалось NotFoundException, возвращено некорректное исключение");
+    }
+
+    @Test
+    public void shouldReturnFilmAfterAdded() {
+        Film validFilm = new Film("Film", "Descr",
+                120, LocalDate.of(2000, 1, 1));
+        Film returnedFilm = filmController.create(validFilm);
+        validFilm.setId(1);
+        assertEquals(validFilm, returnedFilm,
+                "Добавленный фильм не совпадает с изначальным");
+    }
 
 }
--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -3,8 +3,13 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.exception.IncorrectIdentifierException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import ru.yandex.practicum.filmorate.utility.IdentifierGenerator;
 
 import java.time.LocalDate;
@@ -13,42 +18,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserControllerTest {
-//    UserController userController;
-//
-//    @BeforeEach
-//    public void beforeEach() {
-//        userController = new UserController(new IdentifierGenerator());
-//    }
-//
-//    @Test
-//    public void shouldReturnValidationExceptionWhenUserInvalid() {
-//        User loginWithSpaces = new User("mail@mail.ru", "space here",
-//                null, LocalDate.of(2000, 1, 1));
-//        User userWithInvalidId = new User("InvalidId", "IdInvalid",
-//            "some name", LocalDate.of(2000, 1, 1));
-//        userWithInvalidId.setId(-1);
-//
-//        assertThrows(ValidationException.class, () -> userController.create(loginWithSpaces),
-//                "Ожидалось ValidationException, возвращено некорректное исключение");
-//        assertThrows(ValidationException.class, () -> userController.create(userWithInvalidId),
-//                "Ожидалось ValidationException, возвращено некорректное исключение");
-//    }
-//
-//    @Test
-//    public void shouldReturnUserAfterAdded() {
-//        User validUser = new User("user@test.ru", "UserLogin",
-//                "UserName", LocalDate.of(1990, 1, 1));
-//        User returnedUser = userController.create(validUser);
-//        validUser.setId(1);
-//        assertEquals(validUser, returnedUser,
-//                "Добавленный пользователь не совпадает с изначальным");
-//    }
-//
-//    @Test
-//    public void shouldSetNameAsLoginWhenNameIsUndefined() {
-//        User nameless = new User("user@test.ru", "UserLogin", null, LocalDate.of(1990, 1, 1));
-//        userController.create(nameless);
-//        assertEquals(nameless.getLogin(), nameless.getName(),
-//                "Пустому имени не было присоено значение логина пользователя");
-//    }
+    UserController userController;
+    UserStorage userStorage;
+    UserService userService;
+
+    @BeforeEach
+    public void beforeEach() {
+        userStorage = new InMemoryUserStorage(new IdentifierGenerator());
+        userService = new UserService(userStorage);
+        userController = new UserController(userStorage, userService);
+    }
+
+    @Test
+    public void shouldReturnValidationExceptionWhenUserInvalid() {
+        User loginWithSpaces = new User("mail@mail.ru", "space here",
+                null, LocalDate.of(2000, 1, 1));
+        User userWithInvalidId = new User("InvalidId", "IdInvalid",
+            "some name", LocalDate.of(2000, 1, 1));
+        userWithInvalidId.setId(-1);
+
+        assertThrows(ValidationException.class, () -> userController.create(loginWithSpaces),
+                "Ожидалось ValidationException, возвращено некорректное исключение");
+        assertThrows(IncorrectIdentifierException.class, () -> userController.create(userWithInvalidId),
+                "Ожидалось IncorrectIdentifierException, возвращено некорректное исключение");
+    }
+
+    @Test
+    public void shouldReturnUserAfterAdded() {
+        User validUser = new User("user@test.ru", "UserLogin",
+                "UserName", LocalDate.of(1990, 1, 1));
+        User returnedUser = userController.create(validUser);
+        validUser.setId(1);
+        assertEquals(validUser, returnedUser,
+                "Добавленный пользователь не совпадает с изначальным");
+    }
+
+    @Test
+    public void shouldSetNameAsLoginWhenNameIsUndefined() {
+        User nameless = new User("user@test.ru", "UserLogin", null, LocalDate.of(1990, 1, 1));
+        userController.create(nameless);
+        assertEquals(nameless.getLogin(), nameless.getName(),
+                "Пустому имени не было присоено значение логина пользователя");
+    }
 }
